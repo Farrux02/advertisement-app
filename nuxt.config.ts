e762a974e9f94eb7a337f3b5e0bd27ca5321c7ea
Farrux Adxamov <farruh020202@gmail.com>
2025-10-17 11:23:47 +0500

    chore: add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,48 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
     '@nuxt/ui',
-    '@nuxtjs/pwa',
     '@pinia/nuxt',
     '@vueuse/nuxt',
-    '@nuxtjs/tailwindcss',
+    '@vite-pwa/nuxt'
   ],
+  devServer: {
+    port: 3001
+  },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'My Nuxt App',
+      short_name: 'NuxtApp',
+      description: 'Best Nuxt app in the world!',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      orientation: 'portrait',
+      start_url: '/',
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/your-api-domain\.com\/.*$/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api-cache',
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 60 * 60 * 24,
+            },
+          },
+        },
+      ],
+    },
+    devOptions: {
+      enabled: true,
+    },
+  },
 })
